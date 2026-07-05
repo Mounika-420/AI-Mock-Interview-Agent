538a46dcfabd19a33338375c291360382c78f355
Mounika-420 <mounikaselvarajp@gmail.com>
2026-07-05 11:54:57 +0530

    Update login.js

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://ai-mock-interview-agent-pdzb.onrender.com/api";
+const API_BASE = "https://ai-mock-interview-backend-nlrj.onrender.com/api";
 
 const tabLogin = document.getElementById("tab-login");
 const tabRegister = document.getElementById("tab-register");
